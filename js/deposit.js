d3e43252b772d26dfa7cd4b03482aa2c1448f5bc
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-08-18 11:37:04 +0600

    after withdraw total balance and deposit, withdraw field validation added

--- js/deposit.js
+++ js/deposit.js
@@ -3,6 +3,12 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     // step 2: get input field value 
     const depositField = document.getElementById('deposit-field');
     const newDepositAmount = parseFloat(depositField.value);
+    // clear deposit field 
+    depositField.value = '';
+    if (isNaN(newDepositAmount)) {
+        alert('Please enter your deposit amount!');
+        return;
+    }
     // get the deposit total amount 
     const depositTotalElememt = document.getElementById('deposit-total');
     const previousDepositTotal = parseFloat(depositTotalElememt.innerText);
@@ -17,6 +23,5 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
     totalBalanceElement.innerText = currentBalanceTotal;
 
-    // clear deposit field 
-    depositField.value = '';
+
 });
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,6 +2,19 @@ document.getElementById('withdrow-btn').addEventListener('click', function () {
     // gettion withdrow amount from withdraw field 
     const withDrawField = document.getElementById('withdrow-field');
     const newWithDrawAmount = parseFloat(withDrawField.value);
+    const BalanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = parseFloat(BalanceTotalElement.innerText);
+    // clear withDrawField 
+    withDrawField.value = '';
+    // withdraw field validation
+    if (newWithDrawAmount > previousBalanceTotal) {
+        alert('Insufficient Balance!');
+        return;
+    }
+    else if (isNaN(newWithDrawAmount)) {
+        alert('Please enter your withdraw amount!');
+        return;
+    }
 
     // get the withdraw total amount 
     const withdrawTotalElement = document.getElementById('withdraw-total');
@@ -9,6 +22,10 @@ document.getElementById('withdrow-btn').addEventListener('click', function () {
 
     const currentWithdrawTotalBalance = previousWithdrawTotalBalance + newWithDrawAmount;
     withdrawTotalElement.innerText = currentWithdrawTotalBalance;
-    // clear withDrawField 
-    withDrawField.value = '';
+
+    // get total balance 
+
+    const currentBalanceTotal = previousBalanceTotal - newWithDrawAmount;
+    BalanceTotalElement.innerText = currentBalanceTotal;
+
 });
